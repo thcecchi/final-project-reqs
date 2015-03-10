c2c095b44d38f79815565e1122895cb509bafef2
Tyler Cecchi <thcecchi@gmail.com>
2015-03-10 18:40:26 -0400

    initial commit

---
+++ reqs_doc.js
@@ -0,0 +1,136 @@
+build out computer labs for all of the local public housing communities
+
+NBA sports news/pick-em app
+
+this software will:
+
+--- pull in vin videos, news articles and game scores
+--- allow users to comment on all of the above and upvote popular comments
+--- allow users to pick who they think will win games and save their record.
+--- display users with the best pick-record, most upvotes
+
+
+# Requirements Document
+
+
+### Elevator
+
+<p>this nba sports app will add a new level of interaction to following sports as well as facilitate unfiltered conversation around games, news and highlights.</p>
+
+
+### MVP Features
+
+-display daily scoreboard display the current days/s games
+-display vine videos determined by specified hashtags
+-display league news articles
+
+-enable comment function
+-enable upvote function
+
+-enable users to pick who they think will win each day/s games
+-record record of correct/incorrect predictions
+
+-filter content by team name
+
+
+### MVP Stories
+
+
+
+
+### Technologies
+
+- nodejs, express, mongoose, mongodb, angular MEAN
+- heroku, mongolab, mandrill
+- ionic
+
+
+
+### Wireframes
+
+- tbd
+
+### Roadmap (additional features)
+- allow users to pick a favorite team and customize daily content with that of the selected team and who that team/s next game is against
+
+
+## User Stories
+
+      #### Name
+
+        Ability to create delete and upvote comments
+
+
+      #### Value Statement:
+
+      As an NBA fan
+
+      I want to consume content and engage with other fans
+
+      so that I can interact and engage with the league in a unique, clean ui
+
+
+      ####  Assumptions
+
+      ############################################################
+
+      #### Name
+
+        Ability to make predictions of game outcomes
+
+
+      #### Value Statement:
+
+      As an NBA fan
+
+      I want to test my NBA acumen and compare it to other NBA fans
+
+      so that I can interact and engage with the league in a unique, clean ui
+
+
+      ####  Assumptions
+
+      ############################################################
+
+      #### Name
+
+        Ability to view content from Vine, CBS, and NBA
+
+
+      #### Value Statement:
+
+      As an NBA fan
+
+      I want access to an endless stream of content
+
+      so that I can engage with the league in a unique, clean ui
+
+
+      ####  Assumptions
+
+      ############################################################
+
+
+#### Acceptance Criterion
+
+1. user is able to create a comment
+2. user is able to delete a comment
+3. user is able to upvote a comment
+4. user is able to view content
+5. user is able to filter content
+
+#### Notes
+
+
+
+As an \<NBA fan\>,
+
+I want \<to consume content and engage with other fans\>
+
+so that I can \<discuss NBA topics and test my NBA acumen\>
+
+#### Assumptions
+
+#### Acceptance
+
+#### Notes or unknowns
